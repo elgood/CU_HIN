220e75d0a73d65b0667729e593a729b38b2e82f3
Kent <jaca3214@colorado.edu>
2021-02-18 17:47:23 -0700

    Read file capability

--- src/ip_to_ip.py
+++ src/ip_to_ip.py
@@ -1,2 +1,25 @@
 # IP-to-IP matrix for CU HIN class project
 # jaca3214 - Campbell
+
+
+# import libraries
+import argparse
+import numpy as np
+import pandas as pd
+
+
+def main():
+
+    # Process command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputfile', type=str, required=True,
+                        help="The V5 netflow csv file created by 'flow-export -f2'.")
+    flags = parser.parse_args()
+
+    # Open csv file
+    with open(flags.inputfile, 'r') as infile:
+        dfIp2Ip = pd.read_csv(infile, header=None, usecols=[10, 11])    # 10, 11 are 'srcaddr', 'destaddr'
+        print(dfIp2Ip.head())
+
+
+main()
