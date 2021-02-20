ba1c6106d2dd5300c3a5c7a842ffe3b99213bd1e
Kent <jaca3214@colorado.edu>
2021-02-19 18:32:58 -0700

    Create address to int dictionary

--- src/ip_to_ip.py
+++ src/ip_to_ip.py
@@ -1,10 +1,9 @@
 # IP-to-IP matrix for CU HIN class project
-# jaca3214 - Campbell
+# Campbell
 
 
 # import libraries
 import argparse
-import numpy as np
 import pandas as pd
 
 
@@ -18,8 +17,21 @@ def main():
 
     # Open csv file
     with open(flags.inputfile, 'r') as infile:
-        dfIp2Ip = pd.read_csv(infile, header=None, usecols=[10, 11])    # 10, 11 are 'srcaddr', 'destaddr'
-        print(dfIp2Ip.head())
+        ip2ip = pd.read_csv(infile, header=None, usecols=[10, 11], names=['srcaddr', 'destaddr'])
+
+    # Create list of unique addresses
+    srcuniq = ip2ip['srcaddr'].unique()
+    destuniq = ip2ip['destaddr'].unique()
+
+    # Create address dictionaries
+    srcdict = dict(zip(srcuniq, range(len(srcuniq))))
+    destdict = dict(zip(destuniq, range(len(destuniq))))
+
+    # Map back to df
+    ip2ip['srcint'] = ip2ip['srcaddr'].map(srcdict)
+    ip2ip['destint'] = ip2ip['destaddr'].map(destdict)
+
+    print(ip2ip.head())
 
 
 main()
