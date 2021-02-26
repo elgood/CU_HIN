fc8835a467798727304b89a16f692069f9dfaa91
George Varghese <geva2120@colorado.edu>
2021-02-26 03:14:38 -0700

    adding comments

--- src/domain_reg_domain.py
+++ src/domain_reg_domain.py
@@ -1,6 +1,7 @@
 import argparse
 import whois
 
+#getting domain names from the input DNS file
 def getDomainName(filename):
     with open(filename, 'r') as infile:
         domain = infile.readlines()
@@ -10,6 +11,7 @@ def getDomainName(filename):
         domain_name = set(domain_name)
     return domain_name
 
+#doing whois lookup for each of the domain name and printing domain name and registrar
 def whoisLookup(dname):
     for i in dname:
         try:
@@ -28,10 +30,12 @@ def main():
     parser.add_argument('--inputfile', type=str, required=True,
                         help="input csv file")
     FLAGS = parser.parse_args()
-
+    
+    #the whois package only support these TLD
     known_tld = ['com', 'uk', 'ac_uk', 'ar', 'at', 'pl', 'be', 'biz', 'br', 'ca', 'cc', 'cl', 'club', 'cn', 'co', 'jp', 'co_jp', 'cz', 'de', 'store', 'download', 'edu', 'education', 'eu', 'fi', 'fr', 'id', 'in_', 'info', 'io', 'ir', 'is_is', 'it', 'kr', 'kz', 'lt', 'ru', 'lv', 'me', 'mobi', 'mx', 'name', 'net', 'ninja', 'se', 'nu', 'nyc', 'nz', 'online', 'org', 'pharmacy', 'press', 'pw', 'rest', 'ru_rf', 'security', 'sh', 'site', 'space', 'tech', 'tel', 'theatre', 'tickets', 'tv', 'us', 'uz', 'video', 'website', 'wiki', 'xyz']
 
     domain_name_unfiltered = getDomainName(FLAGS.inputfile)
+    #Filtering domain names based on above TLD 
     domain_name = []
     for i in domain_name_unfiltered:
         if i.split(".")[-1] in known_tld:
