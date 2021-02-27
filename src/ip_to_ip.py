81b1b18b200ca8e594b1a7f1b566cda4e37b99d6
Kent <jaca3214@colorado.edu>
2021-02-27 09:21:23 -0700

    Print test of csr

--- src/ip_to_ip.py
+++ src/ip_to_ip.py
@@ -36,7 +36,7 @@ def main():
     # Count co-occurrences
     pairindex = ip2ip.groupby(["srcint", "destint"]).indices
     paircount = {k: len(v) for k, v in pairindex.items()}
-    print('Paircount length:    ', len(paircount))
+    print('Paircount length   :  ', len(paircount))
 
     # Extracting src, dest, counts
     xypair = list(paircount.keys())
@@ -48,6 +48,7 @@ def main():
     ip2ipmatrix = sp.csr_matrix((vals, (rows, cols)))
 
     print('B I G M A T R I X')
+    print(ip2ipmatrix)
 
 
 main()
