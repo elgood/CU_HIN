e75280a0300fc895272f7490abe95eeb90fc21c4
cshank27 <clayton.shank@gmail.com>
2021-03-28 11:38:55 -0600

    uploading our python file for whois. it is a work in progress

---
+++ src/shank_hooper_whois.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import argparse
+import whois
+import time
+import numpy as np
+
+#take data
+
+#when dns is found run whois
+
+#from whois take domain registry domain info
+
+#pass to domain registry_domain.py
