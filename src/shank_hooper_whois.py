5261ba9d490120e8ae2e4851a862b6f970a63d20
cshank27 <clayton.shank@gmail.com>
2021-03-31 19:17:13 -0600

    first commit

--- src/shank_hooper_whois.py
+++ src/shank_hooper_whois.py
@@ -6,12 +6,21 @@ import time
 import numpy as np
 
 #take data
+def processNetflow(netflowInput):
+    #pull collum with dns
 
 
 #when dns is found run whois
+def whoisprocess(dns):
 
 #from whois take domain registry domain info
 
-#pass to domain registry_domain.py
+#pass to domain registry_domain.py george's code
+
+def menu_selection():
+    netflowInput = input('Enter the file name: ')
+
 
 if __name__ == "__main__":
+    #menu selection
+    menu_selection()
