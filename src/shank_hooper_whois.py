594c24b521f49b88624065da790ba4597b8e2c80
Vickie Hooper <vickiehooper@cu-genvpn-tcom-10.180.156.42.int.colorado.edu>
2021-04-05 12:52:50 -0600

    update

--- src/shank_hooper_whois.py
+++ src/shank_hooper_whois.py
@@ -4,23 +4,51 @@ import argparse
 import whois
 import time
 import numpy as np
-
-#take data
-def processNetflow(netflowInput):
-    #pull collum with dns
+import csv
 
 
-#when dns is found run whois
-def whoisprocess(dns):
+def whoIsQuery(domain):
+    data = whois.query(domain)
+    (expiration, registrar, creation) = data.expiration_date, data.registrar, data.creation_date
+    return(expiration, registrar, creation)
+
+#open the file in read only. Search through file for 'A' or 'AAAA'
+#if either are found, so to the 9th column and pull the domain from there
+def getDomainName(filename):
+    with open(filename, 'r') as infile:
+        domain = infile.readlines()
+    domain_name = []
+    for i in range(8,len(domain)):
+        line = domain[i].split()
+        if line[13] == 'A' or line[13] == 'AAAA':
+            domain_name.append(line[9])    
+        print(i)
+
+    domain_names = set(domain_name)
+    return domain_names
+
+def main():
+    netflowInput = input('Enter the file name: ')
+    domain_names = getDomainName(netflowInput)
+
+    #write the domains pulled to a new file and split with lines
+    #file = open('domain_name.txt', 'w')
+    #for domain_name in domain_names:
+    #    file.write(domain_name + '\n')
+    #file.close()
+    #return domain_names
+
+
+    with open('domain_whois.csv', 'w') as file:
+        csv_writer = csv.writer(file, delimiter =',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["expiration_date, registrar, creation_data"])
+        for domain_name in domain_names:
+            whois_data = whoIsQuery(domain_name)
+            csv_writer.writerow([whois_data[0],whois_data[1],whois_data[2]])
+
+if __name__=="__main__":
+    main()
+
 
 #from whois take domain registry domain info
 
-#pass to domain registry_domain.py george's code
-
-def menu_selection():
-    netflowInput = input('Enter the file name: ')
-
-
-if __name__ == "__main__":
-    #menu selection
-    menu_selection()
