b3a0b480123d277db1f293409b367f2a3eda31a3
kentlingcampbell <kentling@kentling.com>
2021-04-06 20:16:12 -0600

    First pass

--- tests/test_ip_to_ip.py
+++ tests/test_ip_to_ip.py
@@ -1,6 +1,18 @@
-from ip_to_ip import ip_to_ip
+import pytest
+import pandas as pd
+import numpy as np
 
-def test_ip_to_ip():
-    return ip_to_ip() 
+from ip_to_ip import createCSR
+# from ip_to_ip import ip_to_ip
+# from ip_to_ip import applyPrune
 
-# fails miserably!
+
+def test_createCSR():
+    data = [[0,1], [0,2], [0,2], [2,0], [2,0], [2,0]]
+    df_test = pd.DataFrame(data,columns=['srcint','destint'])
+    answer = [[0, 1, 2],[0,0,0], [3,0,0]]
+    
+    testData = createCSR(df_test)
+    testData_asArray = testData.toarray()
+
+    assert np.array_equal(answer, testData_asArray)
