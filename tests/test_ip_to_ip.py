84ab08c428e01d7f366d3751ab304a0f41c267e2
kentlingcampbell <kentling@kentling.com>
2021-04-06 20:28:25 -0600

    Test CSR functionality

--- tests/test_ip_to_ip.py
+++ tests/test_ip_to_ip.py
@@ -8,10 +8,12 @@ from ip_to_ip import createCSR
 
 
 def test_createCSR():
-    data = [[0,1], [0,2], [0,2], [2,0], [2,0], [2,0]]
-    df_test = pd.DataFrame(data,columns=['srcint','destint'])
-    answer = [[0, 1, 2],[0,0,0], [3,0,0]]
+    # Setup
+    data = [[0,1],[0,2],[0,2],[2,0],[2,0],[2,0]]                # integer pairs of IP's
+    df_test = pd.DataFrame(data,columns=['srcint','destint'])   # pandas dataframe...
+    answer = [[0,1,2],[0,0,0],[3,0,0]]                          # dense matrix
     
+    # Test 
     testData = createCSR(df_test)
     testData_asArray = testData.toarray()
 
